67e87c915d924751c2c8ba57e58c814e35ae68e8
Gzoref <gzoref@gmail.com>
2019-06-27 22:11:34 -0400

    Landing Page first commit

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<footer>
+      <ul class="footer-icons">
+        <span><a class="fa fa-github" href="https://github.com/Gzoref/vscode_projects"></a></span>
+        <span><a class="fa fa-twitter" href="https://twitter.com/Gzoref"></a></span>
+        <span><a class="fa fa-facebook" href="https://www.facebook.com/gzoref"></a></span>
+        <span><a class="fa fa-envelope" href="mailto:gzoref@gmail.com"></a></span>
+      </ul>
+
+      <p>Made by Geoffrey Zoref &copy; 2019</p>
+    </footer>
+
+    <script src="app.js"></script>
+  </body>
+</html>
+
---
+++ includes/nav.php
@@ -0,0 +1,9 @@
+
+<nav>
+    <ul class="navbar">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="blog.php">Blog</a></li>
+    </ul>
+</nav>
+
